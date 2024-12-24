77f2a4add58a7c74119ba595f3029e3651349575
Blessed Madukoma <blessedmadukoma@gmail.com>
2024-12-24 14:46:53 +0200

    exploratory data analysis

---
+++ code_checker.py
@@ -0,0 +1,9 @@
+import pycodestyle
+
+style_checker = pycodestyle.StyleGuide()
+
+# Run PEP 8 check on multiple files
+result = style_checker.check_files(['nay_pep8.py', 'yay_pep8.py'])
+
+# Print result of PEP 8 style check
+print(result.messages)
